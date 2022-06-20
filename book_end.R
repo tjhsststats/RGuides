161efe715e1246577f28d16e1a10d26effd77358
Kai Sam Ng <kaisamng@gmail.com>
2022-06-19 20:36:53 -0400

    Add section for RStudio setup, added book end and start scripts

---
+++ book_end.R
@@ -0,0 +1,10 @@
+# Create a bibliography with all the packages we are using.
+
+# It would be nice if this only ran when we are building the whole book. How can
+# we check that? In the meantime, we need to add bib/packages to .gitignore. If
+# we did not, then every time someone knitted a chapter, they would need to
+# remember not to check in a new version of bib/packages.bib.
+
+knitr::write_bib(c(.packages(), "bookdown"), "bib/packages.bib")
+
+# lifecycle::last_warnings() Now causes an error. Still necessary?
---
+++ book_start.R
@@ -0,0 +1,27 @@
+# This file is automatically sourced before each chapter is compiled. (Or is it
+# before each session, regardless of the number of chapters being put together?
+# That is, if chapter 4 changes the value for `option("digits")`, will the value
+# be reset for chapter 5? For now, I doubt we need to worry about that since
+# individual chapters rarely mess around with this stuff.)
+
+# This is caused by the setting of "before_chapter_script" in _bookdown.yml.
+
+# Packages used by almost all chapters, and which we don't choose to show to
+# students since the code they see does not make use of them. Should ggthemes be
+# here? Do we need gifski here?
+
+library(ggplot2)
+library(tidyverse)
+library(ggthemes)
+
+options(digits = 2)
+
+# Still need to learn more about figure sizing and display. For now, it sure
+# seems like you always want to set out.width to 100%.
+
+knitr::opts_chunk$set(out.width = "100%")
+
+# I experimented with setting cache = TRUE for all code chunks by this is too
+# dangerous/confusing. Just cache the chunks which take a lot of time.
+
+
